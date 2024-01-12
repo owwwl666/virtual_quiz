178e26c2a4bdb4590bd41b96fe9e5a494ecebdae
owwwl666 <zaharsalam228@gmail.com>
2024-01-12 21:50:52 +1000

    added buttons and a random question when you click on "New Question".

--- tg_bot.py
+++ tg_bot.py
@@ -1,17 +1,28 @@
+import json
+import random
+
+import redis
 from environs import Env
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from buttons import reply_markup
 
 
 def start_callback(update, _):
     """Кнопка /start."""
-    update.message.reply_text("Здравствуйте!")
+    update.message.reply_text("Привет! Я бот для викторин!", reply_markup=reply_markup)
 
 
-def replie_to_message(update, context):
-    update.message.reply_text(update.message.text)
+def reply_message(update, _):
+    if update.message.text == 'Новый вопрос':
+        questions = list(r.hgetall("quiz").keys())
+        question = random.choice(questions)
+        update.message.reply_text(question, reply_markup=reply_markup, parse_mode='HTML')
 
 
 if __name__ == "__main__":
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)
+
     env = Env()
     env.read_env()
 
@@ -19,7 +30,6 @@ if __name__ == "__main__":
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start_callback))
-    dispatcher.add_handler(MessageHandler(Filters.text, replie_to_message))
-
+    dispatcher.add_handler(MessageHandler(Filters.all, reply_message))
 
     updater.start_polling()
