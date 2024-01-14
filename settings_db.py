5f2eb94c7a05ea2252e91ebe9fc9a7e5eb2b387a
owwwl666 <zaharsalam228@gmail.com>
2024-01-15 00:52:57 +1000

    added databases with users and questions.

---
+++ settings_db.py
@@ -0,0 +1,19 @@
+import redis
+from environs import Env
+
+env = Env()
+env.read_env()
+
+questions_redis = redis.Redis(
+    host=env.str('HOST'),
+    port=env.int('PORT'),
+    db=env.int('QUESTIONS_DB'),
+    decode_responses=True
+)
+
+users_redis = redis.Redis(
+    host=env.str('HOST'),
+    port=env.int('PORT'),
+    db=env.int('USERS_DB'),
+    decode_responses=True
+)
