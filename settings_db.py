8da27e29ad5358b48d400281c1fcb913c6b252fb
owwwl666 <zaharsalam228@gmail.com>
2024-01-15 01:20:35 +1000

    added a database for counting user scores.

--- settings_db.py
+++ settings_db.py
@@ -17,3 +17,10 @@ users_redis = redis.Redis(
     db=env.int('USERS_DB'),
     decode_responses=True
 )
+
+points_redis = redis.Redis(
+    host=env.str('HOST'),
+    port=env.int('PORT'),
+    db=env.int('POINTS_DB'),
+    decode_responses=True
+)
