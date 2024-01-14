286e22fdab8bf9be9e37047a91d23d82758558c3
owwwl666 <zaharsalam228@gmail.com>
2024-01-15 01:21:11 +1000

    added counting of correct answers for the user.

--- tg_bot.py
+++ tg_bot.py
@@ -4,13 +4,14 @@ from environs import Env
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
 from buttons import reply_markup
-from settings_db import questions_redis, users_redis
+from settings_db import questions_redis, users_redis, points_redis
 
 NEW_QUESTION, ANSWER, REFUSAL = range(3)
 
 
 def start_callback(update, _):
     """Кнопка /start."""
+    points_redis.set(f'{update.message.chat.id}', '0')
     update.message.reply_text("Привет! Я бот для викторин! Для начала игры нажми кнопку «Новый вопрос»!",
                               reply_markup=reply_markup)
     return NEW_QUESTION
@@ -27,6 +28,8 @@ def handle_new_question_request(update, _):
 def handle_solution_attempt(update, _):
     correct_anwser = questions[users_redis.get(f"{update.message.chat.id}")]
     if update.message.text == correct_anwser:
+        number_points = int(points_redis.get(update.message.chat.id)) + 1
+        points_redis.set(f'{update.message.chat.id}', f'{number_points}')
         update.message.reply_text("Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос».")
         return NEW_QUESTION
     else:
@@ -37,10 +40,16 @@ def handle_solution_attempt(update, _):
 def handle_message_correct_answer(update, context):
     correct_anwser = questions[users_redis.get(f"{update.message.chat.id}")]
     context.bot.send_message(update.effective_chat.id,
-                             f'Правильный ответ:\n{correct_anwser}',
+                             f'<b>Правильный ответ:</b>\n{correct_anwser}',
                              reply_markup=reply_markup, parse_mode='HTML')
     handle_new_question_request(update, context)
     return ANSWER
+
+
+def handle_number_points(update, context):
+    context.bot.send_message(update.effective_chat.id,
+                             f'<b>Количество правильных ответов:</b>\n{points_redis.get(update.message.chat.id)}',
+                             parse_mode='HTML')
 
 
 if __name__ == "__main__":
@@ -52,12 +61,18 @@ if __name__ == "__main__":
     questions = questions_redis.hgetall("quiz")
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start_callback)],
+        entry_points=[
+            CommandHandler('start', start_callback),
+            MessageHandler(Filters.text('Мой счет'), handle_number_points)
+        ],
         states={
-            NEW_QUESTION: [MessageHandler(Filters.text('Новый вопрос'), handle_new_question_request)],
+            NEW_QUESTION: [
+                MessageHandler(Filters.text('Новый вопрос'), handle_new_question_request),
+                MessageHandler(Filters.text('Мой счёт'), handle_number_points)],
             ANSWER: [
+                MessageHandler(Filters.text('Мой счёт'), handle_number_points),
                 MessageHandler(Filters.text('Сдаться'), handle_message_correct_answer),
-                MessageHandler(Filters.text, handle_solution_attempt)
+                MessageHandler(Filters.text, handle_solution_attempt),
             ]
         },
         fallbacks=[]
