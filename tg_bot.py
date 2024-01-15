17078c4dc462db756af821516c8c36671d03797b
owwwl666 <zaharsalam228@gmail.com>
2024-01-15 23:12:08 +1000

    added a /cancel button to exit the quiz. wrote the documentation.

--- tg_bot.py
+++ tg_bot.py
@@ -1,4 +1,5 @@
 import random
+from enum import Enum
 
 from environs import Env
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
@@ -6,57 +7,65 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Conve
 from buttons import reply_markup
 from settings_db import questions_redis, users_redis, points_redis
 
-NEW_QUESTION, ANSWER, REFUSAL = range(3)
+
+class Quiz(Enum):
+    NEW_QUESTION = 1
+    ANSWER = 2
 
 
 def start(update, _):
-    """Кнопка /start."""
+    """Кнопка /start - запуск викторины."""
     points_redis.set(f'{update.message.chat.id}', '0')
     update.message.reply_text("Привет! Я бот для викторин! Для начала игры нажми кнопку «Новый вопрос»!",
                               reply_markup=reply_markup)
-    return NEW_QUESTION
-
-
-def close(update, _):
-    number_points = points_redis.get(update.message.chat.id)
-    update.message.reply_text(
-        f"Спасибо, за участие в Викторине!\nВаше количество правильных ответов: <b>{number_points}</b>",
-        parse_mode='HTML')
-    return ConversationHandler.END
+    return Quiz.NEW_QUESTION
 
 
 def handle_new_question_request(update, _):
+    """Задает пользователю новый рандомный вопрос."""
     question = random.choice(list(questions.keys()))
     users_redis.set(f'{update.message.chat.id}', f'{question}')
     update.message.reply_text(f'{question}',
                               reply_markup=reply_markup, parse_mode='HTML')
-    return ANSWER
+    return Quiz.ANSWER
 
 
 def handle_solution_attempt(update, _):
+    """Проверяет правильность ответа на вопрос."""
     correct_anwser = questions[users_redis.get(f"{update.message.chat.id}")]
     if update.message.text == correct_anwser:
         number_points = int(points_redis.get(update.message.chat.id)) + 1
         points_redis.set(f'{update.message.chat.id}', f'{number_points}')
         update.message.reply_text("Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос».")
-        return NEW_QUESTION
+        return Quiz.NEW_QUESTION
     update.message.reply_text("Неправильно… Попробуешь ещё раз?")
-    return ANSWER
+    return Quiz.ANSWER
 
 
 def handle_message_correct_answer(update, context):
+    """Сообщает пользователю правильный ответ, если он сдается."""
     correct_anwser = questions[users_redis.get(f"{update.message.chat.id}")]
     context.bot.send_message(update.effective_chat.id,
                              f'<b>Правильный ответ:</b>\n{correct_anwser}',
                              reply_markup=reply_markup, parse_mode='HTML')
     handle_new_question_request(update, context)
-    return ANSWER
+    return Quiz.ANSWER
 
 
 def handle_number_points(update, context):
+    """Сообщает пользователю о количестве правильно данных ответов на вопросы."""
     context.bot.send_message(update.effective_chat.id,
                              f'<b>Количество правильных ответов:</b>\n{points_redis.get(update.message.chat.id)}',
                              parse_mode='HTML')
+
+
+def cancel(update, _):
+    """Кнопка /cancel - завершение викторины и выход из нее."""
+    number_points = points_redis.get(update.message.chat.id)
+    update.message.reply_text(
+        f"Спасибо, за участие в Викторине!\nВаше количество правильных ответов: <b>{number_points}</b>",
+        parse_mode='HTML')
+    return ConversationHandler.END
 
 
 if __name__ == "__main__":
@@ -73,16 +82,20 @@ if __name__ == "__main__":
             MessageHandler(Filters.text('Мой счет'), handle_number_points)
         ],
         states={
-            NEW_QUESTION: [
+            Quiz.NEW_QUESTION: [
+                CommandHandler('cancel', cancel),
                 MessageHandler(Filters.text('Новый вопрос'), handle_new_question_request),
-                MessageHandler(Filters.text('Мой счёт'), handle_number_points)],
-            ANSWER: [
+                MessageHandler(Filters.text('Мой счёт'), handle_number_points),
+            ],
+
+            Quiz.ANSWER: [
+                CommandHandler('cancel', cancel),
                 MessageHandler(Filters.text('Мой счёт'), handle_number_points),
                 MessageHandler(Filters.text('Сдаться'), handle_message_correct_answer),
                 MessageHandler(Filters.text, handle_solution_attempt),
             ]
         },
-        fallbacks=[CommandHandler('close', close)]
+        fallbacks=[CommandHandler('cancel', cancel)]
     )
 
     dispatcher = updater.dispatcher
