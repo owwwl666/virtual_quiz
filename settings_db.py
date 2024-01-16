29f4ff76d70bb1dc3fdc025beb351aaca280291e
owwwl666 <zaharsalam228@gmail.com>
2024-01-16 15:17:44 +1000

    set default values ​​for the database.

--- settings_db.py
+++ settings_db.py
@@ -6,21 +6,24 @@ env.read_env()
 
 questions_redis = redis.Redis(
     host=env.str("HOST"),
-    port=env.int("PORT"),
-    db=env.int("QUESTIONS_DB"),
+    port=env.int("PORT", 6379),
+    db=env.int("QUESTIONS_DB", 0),
+    password=env.int("QUESTIONS_DB_PASSWORD", None),
     decode_responses=True,
 )
 
 users_redis = redis.Redis(
     host=env.str("HOST"),
-    port=env.int("PORT"),
-    db=env.int("USERS_DB"),
+    port=env.int("PORT", 6379),
+    db=env.int("QUESTIONS_DB", 1),
+    password=env.int("USERS_DB_PASSWORD", None),
     decode_responses=True,
 )
 
 points_redis = redis.Redis(
     host=env.str("HOST"),
-    port=env.int("PORT"),
-    db=env.int("POINTS_DB"),
+    port=env.int("PORT", 6379),
+    db=env.int("QUESTIONS_DB", 2),
+    password=env.int("POINTS_DB_PASSWORD", None),
     decode_responses=True,
 )
